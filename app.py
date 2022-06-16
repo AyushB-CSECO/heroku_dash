96348973588cea1797315877ea5515b496661ff2
unknown <ayushb3131@gmail.com>
2022-06-16 18:54:31 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import dash
+from dash import html
+from dash import dcc
+import plotly.express as px
+import pandas as pd
+from sklearn import datasets
+
+#Create Data
+housing_dict = datasets.fetch_california_housing()
+data = pd.DataFrame(housing_dict['data'])
+data.columns = housing_dict['feature_names']
+
+#Create a plotly graph to be used by dcc.Graph()
+fig = px.scatter(data,x='AveRooms',y='AveBedrms',title='Relation b/w Rooms & Bedrooms')
+app = dash.Dash(__name__)
+app.title = "House Data Analysis"
+
+app.layout = html.Div(
+         id="app-container"
+        ,children=[
+             html.H1("Relation b/w Rooms & Bedrooms")
+            ,html.P("Results in USD/oz")
+            ,dcc.Graph(figure=fig)
+        ]
+    )
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
